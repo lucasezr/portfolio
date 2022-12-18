c32edf6a5d07de7078fcde37c007dad659d64503
Lucas Rodriguez <lucasezr@hotmail.com>
2022-12-17 22:20:35 -0300

    developing projects carousel. Incomplete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/navbar/Nav";
+import Nav from "./components/navbar/Nav.jsx";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/Projects";
 import Certificates from "./pages/education/Certificates";
 import contactForm from "./pages/contact/Contact_Form";
+import CarouselProject from "./components/carouselProjects/CarouselPjs";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/proyectos" element={<Projects />} />
         <Route exact path="/certificados" element={<Certificates />} />
         <Route exact path="/contacto" element={<contactForm />} />
+        <Route exact path="/prueba" element={<CarouselProject />} />
       </Routes>
     </div>
   );
---
+++ src/components/carouselProjects/CarouselPjs.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "../carouselProjects/carouselPjs.css";
+
+const CarouselPjs = () => {
+  var angle = 0;
+  function galleryspin(sign) {
+    const spinner = document.querySelector(".spinner");
+    if (!sign) {
+      angle = angle - 45;
+    } else {
+      angle = angle - 45;
+    }
+    spinner.setAttribute("style", `transform: rotateY(${+angle + "deg"});`);
+  }
+
+  return (
+    <>
+      <div className="slider">
+        <figure className="spinner">
+          <img
+            src="https://res.cloudinary.com/dr1n4zzvd/image/upload/v1670445553/vector-dogs-pattern_lychwn.jpg"
+            alt="project dogs"
+          ></img>
+          <img
+            src="https://res.cloudinary.com/dr1n4zzvd/image/upload/v1670442379/cafe2_generated_dw1eo4.jpg"
+            alt="project coffeshop"
+          ></img>
+          <img
+            src="https://res.cloudinary.com/dr1n4zzvd/image/upload/v1670445553/vector-dogs-pattern_lychwn.jpg"
+            alt="project dogs"
+          ></img>
+          <img
+            src="https://res.cloudinary.com/dr1n4zzvd/image/upload/v1670442379/cafe2_generated_dw1eo4.jpg"
+            alt="project coffeshop"
+          ></img>
+          <img
+            src="https://res.cloudinary.com/dr1n4zzvd/image/upload/v1670445553/vector-dogs-pattern_lychwn.jpg"
+            alt="project dogs"
+          ></img>
+        </figure>
+      </div>
+      <span
+        className="arrow-one"
+        onClick={(event) => galleryspin(event, " - ")}
+      >
+        &lt;
+      </span>
+
+      <span className="arrow-two" onClick={(event) => galleryspin(event, "")}>
+        &gt;
+      </span>
+    </>
+  );
+};
+
+export default CarouselPjs;
