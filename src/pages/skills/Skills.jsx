973aa90762d45e808116372e22137e7bbf598265
Lucas Rodriguez <lucasezr@hotmail.com>
2022-12-24 21:34:01 -0300

    structuring and styling skills page

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -5,15 +5,21 @@ const skills = () => {
   return (
     <section className="section-skills">
       <div className="conteiner">
-        <article>
-          <div className="back">
+        <article className="back">
+          <div className="back-cont">
             <span>Back-End</span>
-            <div className="Nodejs"></div>
-            <div className="Express"></div>
+            <div className="Nodejs">
+              {" "}
+              <span> NodeJS </span>
+            </div>
+            <div className="Express">
+              {" "}
+              <span> Express </span>
+            </div>
           </div>
         </article>
         <article>
-          <div className="front">
+          <div className="Front">
             <span> Front-End</span>
             <div className="Reactjs"></div>
             <div className="Redux"></div>
@@ -24,20 +30,32 @@ const skills = () => {
         </article>
         <article>
           <div className="db">
-            <span> Database</span>
-            <ul>
-              <li className="PostgreSQL"> </li>
-              <li className="Sequelize"> </li>
-              <li className="Prisma"> </li>
+            <span className="database-text"> Database</span>
+            <ul className="container-li-db">
+              <div className="container-postgresql">
+                <li className="PostgreSQL-icon"></li>
+                <span className="PostgreSQL-text">PostgreSQL</span>
+                <div className="bottom_part"></div>
+              </div>
+              <div className="container-sequelize">
+                <li className="Sequelize-icon"></li>
+                <span className="sequelize-text">Sequelize</span>
+                <div className="bottom_part"></div>
+              </div>
+              <div className="container-prisma">
+                <li className="Prisma-icon"></li>
+                <span className="prisma-text">Prisma</span>
+                <div className="bottom_part"></div>
+              </div>
             </ul>
           </div>
         </article>
       </div>
 
-      <div className="container">
+      {/* <div className="container">
         <div className="logo"> </div>
         <div className="bottom_part"></div>
-      </div>
+      </div> */}
     </section>
   );
 };
